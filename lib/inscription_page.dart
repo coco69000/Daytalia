8bc8a12c4804bf4a2f4d42f0b7e15b40f1a36ef0
coco69000 <corentinparrel2@gmail.com>
2026-05-30 14:14:32 +0200

    gt

--- lib/inscription_page.dart
+++ lib/inscription_page.dart
@@ -59,10 +59,13 @@ class _InscriptionPageState extends State<InscriptionPage> {
   double _qualityOfLife = 50.0;
 
   @override
-  void initState() {
-    super.initState();
-    _currentFlow = widget.initialFlow;
-  }
+void initState() {
+  super.initState();
+  _currentFlow = widget.initialFlow;
+  FirebaseAuth.instance.setSettings(
+    appVerificationDisabledForTesting: true,
+  );
+}
 
   @override
   void dispose() {
